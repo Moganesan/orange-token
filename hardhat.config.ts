23c2e42e1cc0044356fd1a0977d5788eb0cf33ef
moganesan <developer.moganesan@gmail.com>
2024-03-11 11:48:58 +0530

    polygon network added in the hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-toolbox-viem";
 import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
+  networks: {
+    polygon: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [
+        "a398e538b2a032cac4f05357080b03acca3b1510e52c65db27f58a89228fb140",
+      ],
+    },
+  },
   solidity: {
     compilers: [
       {
